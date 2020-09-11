f555ac8b0a48e81801671d9785030e6c4800f95e
Ivan <Vanya.Demyanov@gmai.com>
2020-09-12 00:09:43 +0300

    Api

---
+++ WebApi/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
